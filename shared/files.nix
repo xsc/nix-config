140bd6b792caaaed9ef3ee28b3e4a7f869437d5a
Yannick Scherer <yannick@xsc.dev>
2023-12-05 16:20:59 +0100

    Fetch EurKEY files from a separate Git repository

--- shared/files.nix
+++ shared/files.nix
@@ -1,5 +1,13 @@
 { pkgs, config, lib, ... }:
 
+let
+  eurkey = pkgs.fetchFromGitHub {
+    owner = "xsc";
+    repo = "eurkey-mac";
+    rev= "498a7203309d1cfdb3a81960edc812c38fd8acf3";
+    sha256= "sha256-/Tmm9dkE1HILryzQ9e6or+Z9geCzS5yZ8GLN3ak22pU=";
+  };
+in
 {
   ".lein/profiles.clj" = {
     source = ./config/lein/profiles.clj;
@@ -21,10 +29,10 @@
 
   # EurKEY
   "Library/Keyboard Layouts/EurKEY.icns" = {
-    source = ./config/eurkey/EurKEY.icns;
+    source = eurkey + /EurKEY.icns;
   };
 
   "Library/Keyboard Layouts/EurKEY.keylayout" = {
-    source = ./config/eurkey/EurKEY.keylayout;
+    source = eurkey + /EurKEY.keylayout;
   };
 }
