a8e5909d2f14db1c910a7cdc68ceaf43e9040d75
Yannick Scherer <yannick@xsc.dev>
2023-12-05 15:58:40 +0100

    Format flake.nix using 'nixfmt'

--- flake.nix
+++ flake.nix
@@ -1,4 +1,3 @@
-
 {
   description = "Starter Configuration for NixOS and MacOS";
 
@@ -11,9 +10,7 @@
       url = "github:LnL7/nix-darwin/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nix-homebrew = {
-      url = "github:zhaofengli-wip/nix-homebrew";
-    };
+    nix-homebrew = { url = "github:zhaofengli-wip/nix-homebrew"; };
     homebrew-bundle = {
       url = "github:homebrew/homebrew-bundle";
       flake = false;
@@ -37,29 +34,39 @@
 
     alacritty-theme.url = "github:alexghr/alacritty-theme.nix";
   };
-  outputs = { self, darwin, nix-homebrew, homebrew-bundle, homebrew-core, homebrew-cask, home-manager, nixpkgs, disko, agenix, secrets, alacritty-theme } @inputs:
+  outputs = { self, darwin, nix-homebrew, homebrew-bundle, homebrew-core
+    , homebrew-cask, home-manager, nixpkgs, disko, agenix, secrets
+    , alacritty-theme }@inputs:
     let
       user = "yannick.scherer@futurice.com";
       linuxSystems = [ "x86_64-linux" "aarch64-linux" ];
       darwinSystems = [ "aarch64-darwin" ];
-      forAllLinuxSystems = f: nixpkgs.lib.genAttrs linuxSystems (system: f system);
-      forAllDarwinSystems = f: nixpkgs.lib.genAttrs darwinSystems (system: f system);
-      forAllSystems = f: nixpkgs.lib.genAttrs (linuxSystems ++ darwinSystems) (system: f system);
-      devShell = system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        default = with pkgs; mkShell {
-          nativeBuildInputs = with pkgs; [ bashInteractive git age age-plugin-yubikey ];
-          shellHook = with pkgs; ''
-            export EDITOR=vim
-          '';
+      forAllLinuxSystems = f:
+        nixpkgs.lib.genAttrs linuxSystems (system: f system);
+      forAllDarwinSystems = f:
+        nixpkgs.lib.genAttrs darwinSystems (system: f system);
+      forAllSystems = f:
+        nixpkgs.lib.genAttrs (linuxSystems ++ darwinSystems) (system: f system);
+      devShell = system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          default = with pkgs;
+            mkShell {
+              nativeBuildInputs = with pkgs; [
+                bashInteractive
+                git
+                age
+                age-plugin-yubikey
+              ];
+              shellHook = with pkgs; ''
+                export EDITOR=vim
+              '';
+            };
         };
-      };
-    in
-    {
+    in {
       devShells = forAllSystems devShell;
-      darwinConfigurations = let user = "yannick.scherer@futurice.com"; in {
+      darwinConfigurations = let user = "yannick.scherer@futurice.com";
+      in {
         macos = darwin.lib.darwinSystem {
           system = "aarch64-darwin";
           specialArgs = inputs;
@@ -79,23 +86,27 @@
                 autoMigrate = true;
               };
             }
-            ({ config, pkgs, ...}: { nixpkgs.overlays = [ alacritty-theme.overlays.default ]; })
+            ({ config, pkgs, ... }: {
+              nixpkgs.overlays = [ alacritty-theme.overlays.default ];
+            })
             ./darwin
           ];
         };
       };
-      nixosConfigurations = nixpkgs.lib.genAttrs linuxSystems (system: nixpkgs.lib.nixosSystem {
-        system = system;
-        specialArgs = inputs;
-        modules = [
-          disko.nixosModules.disko
-          home-manager.nixosModules.home-manager {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.${user} = import ./nixos/home-manager.nix;
-          }
-          ./nixos
-        ];
-     });
-  };
+      nixosConfigurations = nixpkgs.lib.genAttrs linuxSystems (system:
+        nixpkgs.lib.nixosSystem {
+          system = system;
+          specialArgs = inputs;
+          modules = [
+            disko.nixosModules.disko
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.${user} = import ./nixos/home-manager.nix;
+            }
+            ./nixos
+          ];
+        });
+    };
 }
