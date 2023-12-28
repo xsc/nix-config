c0563f84bbbd0f512aaa1c07253bc47fd48e57f9
Yannick Scherer <yannick@xsc.dev>
2023-12-28 20:13:22 +0100

    Set pinentry path based on system

--- system/home-manager/files/gpg-agent.nix
+++ system/home-manager/files/gpg-agent.nix
@@ -1,11 +1,19 @@
 { pkgs, ... }:
 
 {
-  ".gnupg/gpg-agent.conf" = {
-    text = ''
-      pinentry-program ${pkgs.pinentry_mac}/${pkgs.pinentry_mac.binaryPath}
-      default-cache-ttl 1800
-      max-cache-ttl 7200
-    '';
-  };
+  ".gnupg/gpg-agent.conf" =
+    let
+      pinentry =
+        if pkgs.stdenv.isDarwin then
+          "${pkgs.pinentry_mac}/${pkgs.pinentry_mac.binaryPath}"
+        else
+          "${pkgs.pinentry-curses}/bin/pinentry-curses";
+    in
+    {
+      text = ''
+        pinentry-program ${pinentry}
+        default-cache-ttl 1800
+        max-cache-ttl 7200
+      '';
+    };
 }
