e9aff55ee7b5ffb2eb49f7a01d423153f1fbecee
Yannick Scherer <yannick@xsc.dev>
2024-07-11 12:10:44 +0200

    Use 'overrideAttrs' to get specific 'duply' version

--- platforms/shared/overlays/duply.nix
+++ platforms/shared/overlays/duply.nix
@@ -5,26 +5,14 @@ let
   minor = "5";
   patch = "3";
   version = "${major}.${minor}.${patch}";
-  duply-pkg = pkgs.fetchzip {
-    url = "https://sourceforge.net/projects/ftplicity/files/duply%20%28simple%20duplicity%29/${major}.${minor}.x/duply_${version}.tgz/download";
-    sha256 = "sha256-RBaVVsc/lsI6cbZyl+dEQ5d+mgKRCtXkZQTi86/OufA=";
-    extension = "tar.gz";
-  };
 in
 (final: prev: {
-  duply = pkgs.stdenv.mkDerivation {
+  duply = prev.duply.overrideAttrs (att: {
     inherit version;
-    name = "duply";
-    src = duply-pkg;
-
-    dontBuild = true;
-    dontFixup = true;
-
-    installPhase = ''
-      runHook preInstall
-      mkdir -p $out/bin
-      install -Dm755 $src/duply $out/bin/duply
-      runHook postInstall
-    '';
-  };
+    src = pkgs.fetchzip {
+      url = "https://sourceforge.net/projects/ftplicity/files/duply%20%28simple%20duplicity%29/${major}.${minor}.x/duply_${version}.tgz/download";
+      sha256 = "sha256-RBaVVsc/lsI6cbZyl+dEQ5d+mgKRCtXkZQTi86/OufA=";
+      extension = "tar.gz";
+    };
+  });
 })
