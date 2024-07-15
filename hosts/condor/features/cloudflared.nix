1aace2c70c46f4e93f772bcb4fedd66a6d091878
Yannick Scherer <yannick@xsc.dev>
2024-07-15 22:29:21 +0200

    Add missing variable

--- hosts/condor/features/cloudflared.nix
+++ hosts/condor/features/cloudflared.nix
@@ -1,9 +1,9 @@
-{ ... }:
+{ config, ... }:
 {
   services.cloudflared = {
     enable = true;
     tunnels = {
-    "f3758bba-06e1-42dc-b269-e60829593e62" = {
+      "f3758bba-06e1-42dc-b269-e60829593e62" = {
         credentialsFile = "${config.age.secrets."cloudflared.pem".path}";
         default = "http_status:404";
         ingress = {
