c33080da0be99087e7f4b90867b8a0ac54815f8a
Yannick Scherer <yannick@xsc.dev>
2024-07-23 15:22:11 +0000

    Make duplicity backup extensible, add vaultwarden backup

--- hosts/condor/features/duplicity.nix
+++ hosts/condor/features/duplicity.nix
@@ -1,6 +1,6 @@
 { config, pkgs, ... }:
 let
-  configFile = pkgs.writeText "conf" ''
+  mkConfigFile = source: target: pkgs.writeText "conf" ''
     source '${config.age.secrets."duplicity.env".path}'
 
     # GPG Options
@@ -8,8 +8,8 @@ let
     GPG_OPTS='--trust-model always --no-tty'
 
     # Source -> Target
-    TARGET="b2://''${B2_KEY_ID}:''${B2_APPLICATION_KEY}@''${B2_BUCKET_NAME}/condor/immich"
-    SOURCE='/var/immich'
+    TARGET="b2://''${B2_KEY_ID}:''${B2_APPLICATION_KEY}@''${B2_BUCKET_NAME}/condor/${target}"
+    SOURCE='${source}'
 
     # Backup Options
     MAX_AGE=3M
@@ -18,17 +18,26 @@ let
   '';
   excludeFile = pkgs.writeText "exclude" "";
   path = [ pkgs.duply pkgs.duplicity pkgs.gnupg pkgs.coreutils pkgs.bash ];
-in
-{
-  systemd.services."duplicity-immich-backup" = {
-    inherit path;
-    script = ''
+
+  mkDuplyScript = source: target: do:
+    let
+      configFile = mkConfigFile source target;
+    in
+    ''
       set -eu
-      mkdir -p /tmp/duply
-      cp -f ${configFile} /tmp/duply/conf
-      cp -f ${excludeFile} /tmp/duply/exclude
-      duply /tmp/duply backup
-      duply /tmp/duply purge --force
+      dir="/tmp/duply/${target}"
+      mkdir -p $dir
+      cp -f ${configFile} $dir/conf
+      cp -f ${excludeFile} $dir/exclude
+      ${do}
+      rm -rf $dir
+    '';
+
+  mkBackupService = { source, target, startAt }: {
+    inherit path startAt;
+    script = mkDuplyScript source target ''
+      duply $dir backup
+      duply $dir purge --force
     '';
     serviceConfig = {
       Type = "oneshot";
@@ -36,17 +45,12 @@ in
     unitConfig = {
       OnFailure = "notify-failure@%i.service";
     };
-    startAt = "*-*-* 02/6:00:00";
   };
 
-  systemd.services."duplicity-immich-backup-verify" = {
-    inherit path;
-    script = ''
-      set -eu
-      mkdir -p /tmp/duply
-      cp -f ${configFile} /tmp/duply/conf
-      cp -f ${excludeFile} /tmp/duply/exclude
-      duply /tmp/duply verify
+  mkVerifyService = { target, startAt }: {
+    inherit path startAt;
+    script = mkDuplyScript "<nothing>" target ''
+      duply $dir verify
     '';
     serviceConfig = {
       Type = "oneshot";
@@ -54,7 +58,31 @@ in
     unitConfig = {
       OnFailure = "notify-failure@%i.service";
     };
-    startAt = "* *-*-01 02:00:00";
   };
+
+in
+{
+
+  # Immich
+  systemd.services."duplicity-immich-backup" =
+    mkBackupService {
+      source = "/var/immich";
+      target = "immich";
+      startAt = "*-*-* 02/6:00:00";
+    };
+
+  systemd.services."duplicity-immich-backup-verify" =
+    mkVerifyService {
+      target = "immich";
+      startAt = "* *-*-01 02:00:00";
+    };
+
+  # Vaultwarden
+  systemd.services."duplicity-vaultwarden-backup" =
+    mkBackupService {
+      source = "/var/lib/bitwarden_rs";
+      target = "vaultwarden";
+      startAt = "*-*-* 03:00:00";
+    };
 }
 
