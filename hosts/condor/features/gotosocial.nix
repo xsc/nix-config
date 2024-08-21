3dd38e43fc3d9a5f1e42123617d5184160f4da91
Yannick Scherer <yannick@xsc.dev>
2024-08-21 06:20:11 +0000

    Setup 'fail2ban' for 'gotosocial'

--- hosts/condor/features/gotosocial.nix
+++ hosts/condor/features/gotosocial.nix
@@ -15,5 +15,29 @@
       instance-languages = [ "en" "de" ];
     };
   };
+
+  services.fail2ban.jails."gotosocial".settings = {
+    enabled = true;
+    filter = "gotosocial";
+    action = "action-ban-gotosocial";
+    maxretry = 3;
+    findtime = 300;
+    bantime = "24h";
+    journalmatch = "_SYSTEMD_UNIT=gotosocial.service";
+  };
+
+  environment.etc = {
+    "fail2ban/filter.d/gotosocial.conf".text = ''
+      [Definition]
+      failregex = ^.*statusCode=401 path=/auth/sign_in clientIP=<ADDR> .* msg=\"Unauthorized:.*$
+      ignoreregex =
+    '';
+
+    "fail2ban/action.d/action-ban-gotosocial.conf".text = ''
+      [Definition]
+      actionban = iptables -I INPUT -p tcp --dport 2286 -m string --algo bm --string 'X-Forwarded-For: <ip>' -j DROP
+      actionunban = iptables -D INPUT -p tcp --dport 2286 -m string --algo bm --string 'X-Forwarded-For: <ip>' -j DROP
+    '';
+  };
 }
 
