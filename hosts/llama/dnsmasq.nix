d5fb0dec1f206b9487e28c7f105a99a1b799761c
Yannick Scherer <yannick@xsc.dev>
2024-10-08 14:01:54 +0200

    Set nameserver on 'llama'

--- hosts/llama/dnsmasq.nix
+++ hosts/llama/dnsmasq.nix
@@ -53,4 +53,6 @@
         ${pkgs.dnsmasq}/bin/dnsmasq --listen-address=10.100.0.102 --port=53 --server=/xsc.dev/10.110.0.1 --server=127.0.0.1 --keep-in-foreground
     '';
   };
+
+  networking.nameservers = [ "127.0.0.1" ];
 }
